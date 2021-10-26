7e872f09662472e31b0465ce0fe62aeaae7dc4fa
kubo-hide-kun <hide44041@gmail.com>
2021-10-27 02:51:41 +0900

    GitHubのリンクの追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,32 @@ import { Header } from "components/Header";
 const HeaderContainer = styled.div`
   display: flex;
   position: fixed;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
   top: 0;
   left: 0;
   right: 0;
   height: 124px;
+`;
+
+const NavigationContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavigationItem = styled.a`
+  margin: 0 24px;
+  color: #2b546a;
+  font-weight: bold;
+  font-size: 1rem;
+  font-family: Lato, "Hiragino Maru Gothic Pro", "Meiryo UI", Meiryo,
+    "MS PGothic", sans-serif;
+  transition: 0.3s;
+  &:hover {
+    color: #30c8d6;
+    text-decoration: none;
+  }
 `;
 
 const BodyContainer = styled.div`
@@ -29,6 +49,24 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       </Head>
       <HeaderContainer>
         <Header />
+        <NavigationContainer>
+          <NavigationItem
+            href="https://github.com/kubo-hide-kun/creative-meetup"
+            target="_blank"
+          >
+            【GitHub】 クライアント
+            <br />
+            Next.js + TS + styled-components
+          </NavigationItem>
+          <NavigationItem
+            href="https://github.com/kubo-hide-kun/creative-meetup-serverless"
+            target="_blank"
+          >
+            【GitHub】 バックエンド
+            <br />
+            Node.js + Lambda + DynamoDB + ServerlessFramework
+          </NavigationItem>
+        </NavigationContainer>
       </HeaderContainer>
       <BodyContainer>
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,84 @@
-import { WordGenerator } from 'components/WordGenerator';
+import { useState, useEffect, ChangeEvent } from "react";
+import styled from "styled-components";
 
-const Home: React.VFC = () => {
-  return <WordGenerator></WordGenerator>;
+import { fetchWord } from "utils/fetchWord";
+
+import { BasicBtn } from "components/BasicBtn";
+import { BasicInput } from "components/BasicInput";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 64px;
+`;
+
+const GenerateResultDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 600px;
+  height: 126px;
+  margin-bottom: 24px;
+  border: 1px solid #e1e5e7;
+  background: #fbf9f5;
+  color: #2b546a;
+  font-weight: bold;
+  font-size: 4rem;
+  font-family: Lato, "Hiragino Maru Gothic Pro", "Meiryo UI", Meiryo,
+    "MS PGothic", sans-serif;
+`;
+
+const NameInput = styled(BasicInput)`
+  width: 600px;
+  margin-bottom: 32px;
+`;
+
+const GenerateBtn = styled(BasicBtn)`
+  width: 196px;
+`;
+
+const HOME: React.VFC = () => {
+  const [name, setName] = useState("");
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    const beforeName = localStorage.getItem("oekaki-user");
+    if (!!beforeName) {
+      setName(beforeName);
+    }
+  }, []);
+
+  const handleClickGenerateBtn = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.currentTarget.value);
+  };
+
+  const handleGenerateBtnClick = () => {
+    if (!name) {
+      return;
+    }
+    const f = async () => {
+      const fetchedWord = await fetchWord(name);
+      setWord(fetchedWord);
+    };
+    f();
+    localStorage.setItem("oekaki-user", name);
+  };
+
+  return (
+    <Container>
+      <GenerateResultDiv>{word}</GenerateResultDiv>
+      <NameInput
+        value={name}
+        placeholder="ユーザー名 (個人が特定できるもの)"
+        onChange={handleClickGenerateBtn}
+      />
+      <GenerateBtn onClick={handleGenerateBtnClick} canClick={!!name}>
+        単語を生成する
+      </GenerateBtn>
+      
+    </Container>
+  );
 };
 
-export default Home;
+export default HOME;
