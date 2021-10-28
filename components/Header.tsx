746588f73850332fd102f2355cc702e87301598c
kubo-hide-kun <hide44041@gmail.com>
2021-10-28 17:13:05 +0900

    feat: 文言修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,10 +15,20 @@ const TitleHead = styled.h1`
   font-size: 2rem;
 `;
 
+const SubTitleHead = styled.div`
+  margin-bottom: 32px;
+  color: #2b546a;
+  font-weight: bold;
+  font-size: 1rem;
+  font-family: Lato, "Hiragino Maru Gothic Pro", "Meiryo UI", Meiryo,
+    "MS PGothic", sans-serif;
+`;
+
 export const Header: React.VFC = () => {
   return (
     <HeaderDiv>
       <TitleHead>お題ジェネレーター</TitleHead>
+      <SubTitleHead>運営側で「どのユーザー」に「どのお題」が割り振られたのかが確認します</SubTitleHead>
     </HeaderDiv>
   );
 };
